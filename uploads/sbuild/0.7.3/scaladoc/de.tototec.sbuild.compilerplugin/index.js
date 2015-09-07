baf5becb62ef3a838a0da81fcee3e77fbef289c1
Tobias Roeser <le.petit.fou@web.de>
2015-09-07 17:32:50 +0200

    upload folder

---
+++ uploads/sbuild/0.7.3/scaladoc/de.tototec.sbuild.compilerplugin/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"de" : [], "de.tototec" : [], "de.tototec.sbuild" : [], "de.tototec.sbuild.compilerplugin" : [{"class" : "de\/tototec\/sbuild\/compilerplugin\/AnalyzeTypesPlugin.html", "name" : "de.tototec.sbuild.compilerplugin.AnalyzeTypesPlugin"}]};
